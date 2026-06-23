c2c618989101589b34a3ccdbaac29094999cf7a9
michal <mkslotwinski@gmail.com>
2026-06-23 11:36:02 +0100

    typo fix

--- terraform/rg.tf
+++ terraform/rg.tf
@@ -8,4 +8,4 @@ resource "azurerm_resource_group" "rg_windmill" {
 import {
   to = azurerm_resource_group.rg_windmill
   id = "/subscriptions/d109d869-87b3-4bf5-b9de-1380f51a8181/resourceGroups/rg-windmill"
-  }
+}
